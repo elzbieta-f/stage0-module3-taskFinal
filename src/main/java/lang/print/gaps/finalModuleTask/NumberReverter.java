7649c1ba105ab855de0181a1414333bfd3d701fd
elzbieta-f <elzbieta.fulga@gmail.com>
2022-07-03 17:40:26 +0300

    Final task submission

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first=number%10;
+        int second=(number%100)/10;
+        int third=number/100;
+
+        int result=first*100+second*10+third;
+
+        System.out.println(result);
     }
 }
